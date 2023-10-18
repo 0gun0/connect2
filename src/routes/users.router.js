16b9e434a00cbe9cd2ea39d574a269d4c177a6bf
geonnyeong oh <geonnyeongoh@geonnyeongs-MacBook-Air.local>
2023-10-18 10:37:23 +0900

    cached

---
+++ src/routes/users.router.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import {prisma} from '../utils/prisma/index.js';
+
+
+const router = express.Router(); 
+
+/** 사용자 회원가입 API **/
+
+router.post('/sign-up', async(req, res, next)=>{
+
+// 1. `email`, `password`, `name`, `age`, `gender`, `profileImage`를 **body**로 전달받습니다.
+const {email, password, name, age, gender, profileImage} = req.body;
+// 2. 동일한 `email`을 가진 사용자가 있는지 확인합니다.
+const isExistUser = await prisma.users.findFirst({
+    where: {email},
+});
+if(isExistUser){
+    return res.status(409).json({message : '이미 존재하는 이메일입니다.'});
+}
+// 3. **Users** 테이블에 `email`, `password`를 이용해 사용자를 생성합니다.
+const hashedPassword = await bcrypt.hash(password, 10);
+
+const user = await prisma.users.create({
+    data: {
+        email, 
+        password: hashedPassword,
+    }
+})
+// 4. **UserInfos** 테이블에 `name`, `age`, `gender`, `profileImage`를 이용해 사용자 정보를 생성합니다.
+const userInfo = await prisma.userInfos.create({
+    data:{
+        UserId: user.userId,
+        name,
+        age,
+        gender : gender.toUpperCase(),
+        profileImage,
+    }
+})
+return res.status(201).json({message:'회원가입이 완료되었습니다.'})
+});
+
+/** 사용자 로그인 API **/
+router.post('/sign-in', async (req, res, next)=>{
+    const {email, password} = req.body;
+
+const user = await prisma.users.findFirst({where: {email}});
+if (!user){
+    return res.status(401).json({message : '존재하지 않는 이메일입니다.'});
+}
+const result = await bcrypt.compare(password, user.password);
+if(!result){
+    return res.status(401).json({message : '비밀번호가 일치하지 않습니다.'})
+}
+const token = jwt.sign(
+    {
+        userId: user.userId,
+    },
+    'customized_secret_key',
+)
+res.cookie('authorization',`Bearer ${token}`);
+
+return res.status(200).json({message : '로그인 성공'});
+})
+
+export default router;
+
+
